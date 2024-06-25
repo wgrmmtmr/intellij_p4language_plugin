f71305aa1e21ed3b03366de9465da296d6f2a52e
GitHub Action <action@github.com>
2024-06-25 02:59:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/wgrmmtmr/intellijp4languageplugin/MyBundle.kt
+++ src/main/kotlin/com/github/wgrmmtmr/intellijp4languageplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wgrmmtmr.intellijp4languageplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/wgrmmtmr/intellijp4languageplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/wgrmmtmr/intellijp4languageplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.wgrmmtmr.intellijp4languageplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wgrmmtmr.intellijp4languageplugin.MyBundle
+import com.github.wgrmmtmr.intellijp4languageplugin.services.MyProjectService
 import javax.swing.JButton
 
 
